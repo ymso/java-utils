31e0669e8cd81656c157ceb26fbe7b1dbbbac84e
wb_gaojie02 <wb_gaojie02@meituan.com>
2022-07-18 13:57:14 +0800

    feature: add nacos

---
+++ nacos-client/src/main/java/org/example/NacosProviderBootstrap.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: 高杰
+ * Date: 2022/7/18
+ * Desc:
+ */
+@SpringBootApplication
+public class NacosProviderBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderBootstrap.class, args);
+    }
+}
